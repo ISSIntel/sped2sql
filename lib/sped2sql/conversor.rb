3627b447f48434b01a0516ec7d0da681348400bf
Ricardo Cesar de Oliveira <rcoproc@gmail.com>
2018-09-30 19:31:40 -0400

    Binding.pry Added

--- lib/sped2sql/conversor.rb
+++ lib/sped2sql/conversor.rb
@@ -1,9 +1,10 @@
+require "pry"
 # -*- encoding: utf-8 -*-
 module SPED2SQL
   class Conversor < Pipeline::Base
     include Layout
 
-    attr_reader :fonte, :template, :saida, :memoria, :options
+    attr_reader :fonte, :template, :saida, :memoria, :options, :errors
 
     def initialize(fonte, template, options = {})
       @fonte      = fonte
@@ -11,6 +12,7 @@ module SPED2SQL
       @saida      = []
       @memoria    = Hash.new { |k, v| k[v] = [] }
       @options    = options
+      @errors     = []
 
       valida_arquivo(@fonte)
       valida_arquivo(@template)
@@ -29,29 +31,38 @@ module SPED2SQL
     def converter!
       mapa  = Mapa.carrega!(@template)
       dados = IO.read(fonte, encoding: 'ISO-8859-1').gsub("'", '"')
+      row_count = 0
 
       CSV.parse(dados, col_sep: '|', quote_char: "'") do |row|
+        row_count += 1
         # pula linha se o registro nao existe no mapa
         next unless mapa.has_key?(row[1])
 
-        # O primeiro e o ultimo item de uma linha no SPED sempre eh nulo
-        linha = row.clone[1..-2]
+        begin
+          # O primeiro e o ultimo item de uma linha no SPED sempre eh nulo
+          linha = row.clone[1..-2]
 
-        # Executa o pipe
-        pipe = execute({ original: linha,
-                         final:    linha,
-                         mapa:     mapa,
-                         memoria:  @memoria,
-                         saida:    @saida,
-                         options:  @options })
+          # Executa o pipe
+          pipe = execute({ original: linha,
+                           final:    linha,
+                           mapa:     mapa,
+                           memoria:  @memoria,
+                           saida:    @saida,
+                           options:  @options })
 
-        @saida << pipe[:final]
-        @memoria[linha.first] << pipe[:final]
+          @saida << pipe[:final]
+          @memoria[linha.first] << pipe[:final]
 
-        # Para um arquivo completo do SPED, 9999 eh o ultimo registro.
-        # termina a leitura do arquivo no registro 9999 evitando ler
-        # linhas em branco ou assinatura digital
-        break if linha[0].to_i == 9999
+          # Para um arquivo completo do SPED, 9999 eh o ultimo registro.
+          # termina a leitura do arquivo no registro 9999 evitando ler
+          # linhas em branco ou assinatura digital
+          break if linha[0].to_i == 9999
+
+        # Guarda em @errors as mensagens e pula para a próxima linha
+        rescue => e
+          binding.pry
+          @errors << "#{row_count} - #{e.message}"
+        end
       end
     end
 
